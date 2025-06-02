15e40ad5dd6227dce03a0acae5fa6e318e6b5fa8
jantoniomg <jantoniomg72@gmail.com>
2025-06-02 14:53:39 +0200

    header y footer

--- moneygold/header y footer/header.gd
+++ moneygold/header y footer/header.gd
@@ -6,7 +6,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	dineroF_label.text = str(CargarDatos.dineroFisico)
-	CargarDatos.cash_updated.connect(actualizado_dineroFisico)
+	CargarDatos.cash_updated.connect(new_cash)
 
 func new_cash(new_value):
 	dineroF_label.text = str(CargarDatos.dineroFisico)
--- moneygold/menus/script/inicio_sesion.gd
+++ moneygold/menus/script/inicio_sesion.gd
@@ -1,43 +1,58 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+# Funcion llamada al inicio
 func _ready():
-	pass
-
+	#Si haces login llamas a on_iniciar_sesion_succeeded
+	Firebase.Auth.login_succeeded.connect(on_iniciar_sesion_succeeded)
+	
+	#Si te registras llamas a on_registrarse_succeeded
+	Firebase.Auth.signup_succeeded.connect(on_registrarse_succeeded)
+	
+	#Si falla el login llamas a on_iniciar_sesion_failed
+	Firebase.Auth.login_failed.connect(on_iniciar_sesion_failed)
+	
+	#Si haces login llamas a on_registrarse_failed
+	Firebase.Auth.signup_failed.connect(on_registrarse_failed)
+# En caso de que ya hallas accedido con anterioridad lo detectaremos del sistema
+	if Firebase.Auth.check_auth_file():
+		$VBoxContainer/Estado.text = "Accedido"
+		get_tree().change_scene_to_file("res://menus/MenuPrincipal.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	pass
 
-
+# Funcion para hacer login 
 func _on_iniciar_sesion_pressed():
-	var email = %Usuario.text
-	var password = %Contraseña.text
+	var email = $VBoxContainer/Usuario.text
+	var password = $VBoxContainer/Contraseña.text
 	Firebase.Auth.login_with_email_and_password(email, password)
+	$VBoxContainer/Estado.text = "Accediendo"
 
-
+# Funcion para registrarse
 func _on_registrarse_pressed() -> void:
-	var email = %Usuario.text
-	var password = %Contraseña.text
+	var email = $VBoxContainer/Usuario.text
+	var password = $VBoxContainer/Contraseña.text
 	Firebase.Auth.signup_with_email_and_password(email, password)
-	
-func on_login_succeeded(auth):
+	$VBoxContainer/Estado.text = "Registrado"
+
+func on_iniciar_sesion_succeeded(auth):
 	print(auth)
 	Firebase.Auth.save_auth(auth)
-	get_tree().change_scene_to_file("")
-	
-func on_signup_succeeded(auth):
+	get_tree().change_scene_to_file("res://menus/MenuPrincipal.tscn")
+
+func on_registrarse_succeeded(auth):
 	print(auth)
 	Firebase.Auth.save_auth(auth)
-	get_tree().change_scene_to_file(" ")
-	
-func on_login_failed(error_code, message):
+	get_tree().change_scene_to_file("res://menus/MenuPrincipal.tscn")
+
+func on_iniciar_sesion_failed(error_code, message):
 	print(error_code)
 	print(message)
-	%Estado.text = "Login failed. Error: %s" % message
-	
-func on_signup_failed(error_code, message):
+	$VBoxContainer/Estado.text = "Acceso fallido"
+
+func on_registrarse_failed(error_code, message):
 	print(error_code)
 	print(message)
-	%Estado.text = "Sign up failed. Error: %s" % message
+	$VBoxContainer/Estado.text = "Registro fallido"
